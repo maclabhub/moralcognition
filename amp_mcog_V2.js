f11614dbd493bb8212b46f88c5c5a7f7876724aa
maclabhub <80420614+maclabhub@users.noreply.github.com>
2021-05-12 19:37:28 -0400

    amp v2

---
+++ amp_mcog_V2.js
@@ -0,0 +1,316 @@
+define(['pipAPI', 'https://maclabhub.github.io/moralcognition/qamp_mcog.js'], function(APIConstructor, ampExtension){
+
+	var API = new APIConstructor();
+
+
+	return ampExtension({
+		primeCats :  [
+			{
+				nameForFeedback : 'donate',  //Will be used in the user feedback
+				nameForLogging : 'donate', //Will be used in the logging
+				//An array of all media objects for this category.
+				mediaArray : [
+				    {word : 'donate'}]
+
+			},
+			{
+				nameForFeedback : 'contribute',  //Will be used in the user feedback
+				nameForLogging : 'contribute', //Will be used in the logging
+				//An array of all media objects for this category.
+				mediaArray : [
+				    {word : 'contribute'}]
+
+			},
+			{
+				nameForFeedback : 'paper',  //Will be used in the user feedback
+				nameForLogging : 'paper', //Will be used in the logging
+				//An array of all media objects for this category.
+				mediaArray : [
+				    {word : 'paper'}]
+
+			},
+			{
+				nameForFeedback : 'provide',  //Will be used in the user feedback
+				nameForLogging : 'provide', //Will be used in the logging
+				//An array of all media objects for this category.
+				mediaArray : [
+				    {word : 'provide'}]
+
+	},
+			{
+				nameForFeedback : 'pen',  //Will be used in the user feedback
+				nameForLogging : 'pen', //Will be used in the logging
+				//An array of all media objects for this category.
+				mediaArray : [
+				    {word : 'pen'}]
+
+			},
+			{
+				nameForFeedback : 'help',  //Will be used in the user feedback
+				nameForLogging : 'help', //Will be used in the logging
+				//An array of all media objects for this category.
+				mediaArray : [
+				    {word : 'help'}]
+
+			},
+			{
+				nameForFeedback : 'shelf',  //Will be used in the user feedback
+				nameForLogging : 'shelf', //Will be used in the logging
+				//An array of all media objects for this category.
+				mediaArray : [
+				    {word : 'shelf'}]
+
+			},
+			{
+				nameForFeedback : 'generous',  //Will be used in the user feedback
+				nameForLogging : 'generous', //Will be used in the logging
+				//An array of all media objects for this category.
+				mediaArray : [
+				    {word : 'generous'}]
+
+			},
+			{
+				nameForFeedback : 'wall',  //Will be used in the user feedback
+				nameForLogging : 'wall', //Will be used in the logging
+				//An array of all media objects for this category.
+				mediaArray : [
+				    {word : 'wall'}]
+
+			},
+			{
+				nameForFeedback : 'charity',  //Will be used in the user feedback
+				nameForLogging : 'charity', //Will be used in the logging
+				//An array of all media objects for this category.
+				mediaArray : [
+				    {word : 'charity'}]
+
+			},
+			{
+				nameForFeedback : 'floor',  //Will be used in the user feedback
+				nameForLogging : 'floor', //Will be used in the logging
+				//An array of all media objects for this category.
+				mediaArray : [
+				    {word : 'floor'}]
+
+			},
+			{
+				nameForFeedback : 'non-profit',  //Will be used in the user feedback
+				nameForLogging : 'non-profit', //Will be used in the logging
+				//An array of all media objects for this category.
+				mediaArray : [
+				    {word : 'non-profit'}]
+
+			},
+			{
+				nameForFeedback : 'share',  //Will be used in the user feedback
+				nameForLogging : 'share', //Will be used in the logging
+				//An array of all media objects for this category.
+				mediaArray : [
+				    {word : 'share'}]
+
+			},
+			{
+				nameForFeedback : 'give',  //Will be used in the user feedback
+				nameForLogging : 'give', //Will be used in the logging
+				//An array of all media objects for this category.
+				mediaArray : [
+				    {word : 'give'}]
+
+			},
+			{
+				nameForFeedback : 'support',  //Will be used in the user feedback
+				nameForLogging : 'support', //Will be used in the logging
+				//An array of all media objects for this category.
+				mediaArray : [
+				    {word : 'support'}]
+
+			},
+			{
+				nameForFeedback : 'sponsor',  //Will be used in the user feedback
+				nameForLogging : 'sponsor', //Will be used in the logging
+				//An array of all media objects for this category.
+				mediaArray : [
+				    {word : 'sponsor'}]
+
+			},
+			{
+				nameForFeedback : 'charitable',  //Will be used in the user feedback
+				nameForLogging : 'charitable', //Will be used in the logging
+				//An array of all media objects for this category.
+				mediaArray : [
+				    {word : 'charitable'}]
+
+			},
+			{
+				nameForFeedback : 'sharing',  //Will be used in the user feedback
+				nameForLogging : 'sharing', //Will be used in the logging
+				//An array of all media objects for this category.
+				mediaArray : [
+				    {word : 'sharing'}]
+
+			},
+			{
+				nameForFeedback : 'compassion',  //Will be used in the user feedback
+				nameForLogging : 'compassion', //Will be used in the logging
+				//An array of all media objects for this category.
+				mediaArray : [
+				    {word : 'compassion'}]
+
+			},
+			{
+				nameForFeedback : 'withhold',  //Will be used in the user feedback
+				nameForLogging : 'withhold', //Will be used in the logging
+				//An array of all media objects for this category.
+				mediaArray : [
+				    {word : 'withhold'}]
+
+			},
+			{
+				nameForFeedback : 'take',  //Will be used in the user feedback
+				nameForLogging : 'take', //Will be used in the logging
+				//An array of all media objects for this category.
+				mediaArray : [
+				    {word : 'take'}]
+
+			},
+			{
+				nameForFeedback : 'table',  //Will be used in the user feedback
+				nameForLogging : 'table', //Will be used in the logging
+				//An array of all media objects for this category.
+				mediaArray : [
+				    {word : 'table'}]
+
+			},
+			{
+				nameForFeedback : 'uncharitable',  //Will be used in the user feedback
+				nameForLogging : 'uncharitable', //Will be used in the logging
+				//An array of all media objects for this category.
+				mediaArray : [
+				    {word : 'uncharitable'}]
+
+			},
+			{
+				nameForFeedback : 'chair',  //Will be used in the user feedback
+				nameForLogging : 'chair', //Will be used in the logging
+				//An array of all media objects for this category.
+				mediaArray : [
+				    {word : 'chair'}]
+
+			},
+			{
+				nameForFeedback : 'keep',  //Will be used in the user feedback
+				nameForLogging : 'keep', //Will be used in the logging
+				//An array of all media objects for this category.
+				mediaArray : [
+				    {word : 'keep'}]
+
+			},
+			{
+				nameForFeedback : 'desk',  //Will be used in the user feedback
+				nameForLogging : 'desk', //Will be used in the logging
+				//An array of all media objects for this category.
+				mediaArray : [
+				    {word : 'desk'}]
+
+			},
+			{
+				nameForFeedback : 'greedy',  //Will be used in the user feedback
+				nameForLogging : 'greedy', //Will be used in the logging
+				//An array of all media objects for this category.
+				mediaArray : [
+				    {word : 'greedy'}]
+
+			},
+			{
+				nameForFeedback : 'lamp',  //Will be used in the user feedback
+				nameForLogging : 'lamp', //Will be used in the logging
+				//An array of all media objects for this category.
+				mediaArray : [
+				    {word : 'lamp'}]
+
+			},
+			{
+				nameForFeedback : 'apathy',  //Will be used in the user feedback
+				nameForLogging : 'apathy', //Will be used in the logging
+				//An array of all media objects for this category.
+				mediaArray : [
+				    {word : 'apathy'}]
+
+			},
+			{
+				nameForFeedback : 'pencil',  //Will be used in the user feedback
+				nameForLogging : 'pencil', //Will be used in the logging
+				//An array of all media objects for this category.
+				mediaArray : [
+				    {word : 'pencil'}]
+
+			},
+			{
+				nameForFeedback : 'for me',  //Will be used in the user feedback
+				nameForLogging : 'for me', //Will be used in the logging
+				//An array of all media objects for this category.
+				mediaArray : [
+				    {word : 'for me'}]
+
+			},
+			{
+				nameForFeedback : 'retain',  //Will be used in the user feedback
+				nameForLogging : 'retain', //Will be used in the logging
+				//An array of all media objects for this category.
+				mediaArray : [
+				    {word : 'retain'}]
+
+			},
+			{
+				nameForFeedback : 'ceiling',  //Will be used in the user feedback
+				nameForLogging : 'ceiling', //Will be used in the logging
+				//An array of all media objects for this category.
+				mediaArray : [
+				    {word : 'ceiling'}]
+
+			},
+			{
+				nameForFeedback : 'for-profit',  //Will be used in the user feedback
+				nameForLogging : 'for-profit', //Will be used in the logging
+				//An array of all media objects for this category.
+				mediaArray : [
+				    {word : 'for-profit'}]
+
+			},
+			{
+				nameForFeedback : 'book',  //Will be used in the user feedback
+				nameForLogging : 'book', //Will be used in the logging
+				//An array of all media objects for this category.
+				mediaArray : [
+				    {word : 'book'}]
+
+			},
+			{
+				nameForFeedback : 'selfish',  //Will be used in the user feedback
+				nameForLogging : 'selfish', //Will be used in the logging
+				//An array of all media objects for this category.
+				mediaArray : [
+				    {word : 'selfish'}]
+
+			},
+			{
+				nameForFeedback : 'mine',  //Will be used in the user feedback
+				nameForLogging : 'mine', //Will be used in the logging
+				//An array of all media objects for this category.
+				mediaArray : [
+				    {word : 'mine'}]
+
+			}
+		],
+
+		examplePrimeStimulus :
+		{
+			nameForLogging : 'examplePrime', //Will be used in the logging
+			//An array of all media objects for this category.
+			mediaArray : [{word : 'table'}, {word : 'chair'}, {word : 'desk'}, {word : 'paper'}, {word : 'lamp'}, {word : 'pen'}, {word : 'pencil'}, {word : 'shelf'}, {word : 'floor'}, {word : 'wall'}]
+		},
+
+		base_url : 'https://maclabhub.github.io/moralcognition/ampimages'
+
+	});
+});
