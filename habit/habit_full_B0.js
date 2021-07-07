914e8a5c0a81ad194d93a0dec4511829b6f29668
maclabhub <80420614+maclabhub@users.noreply.github.com>
2021-07-07 16:00:08 -0400

    update

--- habit/habit_full_B0.js
+++ habit/habit_full_B0.js
@@ -29,7 +29,7 @@ define(['pipAPI','underscore'], function(APIConstructor, _) {
 				borderColor: 'lightblue'
 			},
 			//The CSS for all the prime stimuli.
-			primeStimulusCSS : {color:'#0000FF','font-size':'2.3em'},
+			primeStimulusCSS : {color:'#000000','font-size':'2.3em'},
 			//The prime categories.
 			primeCats :  [
 				{
@@ -51,7 +51,7 @@ define(['pipAPI','underscore'], function(APIConstructor, _) {
 				mediaArray : [{word : 'Table'}, {word : 'Chair'}]
 			},
 			//The CSS for all the target stimuli (usually irrelevant because the targets are Chinese pictographs.
-			targetStimulusCSS : {color:'#0000FF','font-size':'2.3em'},
+			targetStimulusCSS : {color:'#000000','font-size':'2.3em'},
 			//The prime categories.
 			targetCats :  [
 				{
