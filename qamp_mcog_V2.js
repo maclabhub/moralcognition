988d1d82c7074a58086cf493709fb118f84a5eb0
maclabhub <80420614+maclabhub@users.noreply.github.com>
2021-07-16 01:45:31 -0400

    update

--- qamp_mcog_V2.js
+++ qamp_mcog_V2.js
@@ -107,7 +107,7 @@ define(['pipAPI','underscore'], function(APIConstructor, _) {
 				image : 'https://baranan.github.io/minno-tasks/images/ampImages'
 			},
 
-			trialsInBlock : [37, 37, 37], //Number of trials in each block
+			trialsInBlock : [35, 35, 35], //Number of trials in each block
 			trialsInExample : 10, //Change to 0 if you don't want an example block
 
 			//Duration parameters.
