cf26a09b45a2b88f2dc45d43c0890013248c04e4
maclabhub <80420614+maclabhub@users.noreply.github.com>
2021-07-30 01:33:44 -0400

    added blocks

---
+++ testing/amp_habit_test_b2.js
@@ -0,0 +1,293 @@
+define(['pipAPI', 'https://maclabhub.github.io/moralcognition/testing/ampfull_habit_test.js'], function(APIConstructor, ampExtension){
+
+	var API = new APIConstructor();
+
+
+	return ampExtension({
+		primeCats :  [
+			{
+				nameForFeedback : 'h_1',  //Will be used in the user feedback
+				nameForLogging : 'h_1', //Will be used in the logging
+				//An array of all media objects for this category.
+				mediaArray : [
+				    {word : 'donate money'}]
+
+			},
+			{
+				nameForFeedback : 'h_2',  //Will be used in the user feedback
+				nameForLogging : 'h_2', //Will be used in the logging
+				//An array of all media objects for this category.
+				mediaArray : [
+				    {word : 'contribute X'}]
+
+			},
+			{
+				nameForFeedback : 'h_3',  //Will be used in the user feedback
+				nameForLogging : 'h_3', //Will be used in the logging
+				//An array of all media objects for this category.
+				mediaArray : [
+				    {word : 'paper clip'}]
+
+			},
+			{
+				nameForFeedback : 'h_4',  //Will be used in the user feedback
+				nameForLogging : 'h_4', //Will be used in the logging
+				//An array of all media objects for this category.
+				mediaArray : [
+				    {word : 'provide that'}]
+
+	},
+			{
+				nameForFeedback : 'h_5',  //Will be used in the user feedback
+				nameForLogging : 'h_5', //Will be used in the logging
+				//An array of all media objects for this category.
+				mediaArray : [
+				    {word : 'pen'}]
+
+			},
+			{
+				nameForFeedback : 'h_6',  //Will be used in the user feedback
+				nameForLogging : 'h_6', //Will be used in the logging
+				//An array of all media objects for this category.
+				mediaArray : [
+				    {word : 'help them'}]
+
+			},
+			{
+				nameForFeedback : 'h_7',  //Will be used in the user feedback
+				nameForLogging : 'h_7', //Will be used in the logging
+				//An array of all media objects for this category.
+				mediaArray : [
+				    {word : 'top shelf'}]
+
+			},
+			{
+				nameForFeedback : 'h_8',  //Will be used in the user feedback
+				nameForLogging : 'h_8', //Will be used in the logging
+				//An array of all media objects for this category.
+				mediaArray : [
+				    {word : 'generous man'}]
+
+			},
+			{
+				nameForFeedback : 'h_9',  //Will be used in the user feedback
+				nameForLogging : 'h_9', //Will be used in the logging
+				//An array of all media objects for this category.
+				mediaArray : [
+				    {word : 'wall paper'}]
+
+			},
+			{
+				nameForFeedback : 'h_10',  //Will be used in the user feedback
+				nameForLogging : 'h_10', //Will be used in the logging
+				//An array of all media objects for this category.
+				mediaArray : [
+				    {word : 'give to charity'}]
+
+			},
+			{
+				nameForFeedback : 'h_11',  //Will be used in the user feedback
+				nameForLogging : 'h_11', //Will be used in the logging
+				//An array of all media objects for this category.
+				mediaArray : [
+				    {word : 'mow your neighbor’s grass'}]
+
+			},
+			{
+				nameForFeedback : 'h_12',  //Will be used in the user feedback
+				nameForLogging : 'h_12', //Will be used in the logging
+				//An array of all media objects for this category.
+				mediaArray : [
+				    {word : 'take a picture of yourself'}]
+
+			},
+			{
+				nameForFeedback : 'h_13',  //Will be used in the user feedback
+				nameForLogging : 'h_13', //Will be used in the logging
+				//An array of all media objects for this category.
+				mediaArray : [
+				    {word : 'give'}]
+
+			},
+			{
+				nameForFeedback : 'h_14',  //Will be used in the user feedback
+				nameForLogging : 'h_14', //Will be used in the logging
+				//An array of all media objects for this category.
+				mediaArray : [
+				    {word : 'support'}]
+
+			},
+			{
+				nameForFeedback : 'h_15',  //Will be used in the user feedback
+				nameForLogging : 'h_15', //Will be used in the logging
+				//An array of all media objects for this category.
+				mediaArray : [
+				    {word : 'sponsor'}]
+
+			},
+			{
+				nameForFeedback : 'h_16',  //Will be used in the user feedback
+				nameForLogging : 'h_16', //Will be used in the logging
+				//An array of all media objects for this category.
+				mediaArray : [
+				    {word : 'charitable'}]
+
+			},
+			{
+				nameForFeedback : 'h_17',  //Will be used in the user feedback
+				nameForLogging : 'h_17', //Will be used in the logging
+				//An array of all media objects for this category.
+				mediaArray : [
+				    {word : 'sharing'}]
+
+			},
+			{
+				nameForFeedback : 'h_18',  //Will be used in the user feedback
+				nameForLogging : 'h_18', //Will be used in the logging
+				//An array of all media objects for this category.
+				mediaArray : [
+				    {word : 'compassion'}]
+
+			},
+			{
+				nameForFeedback : 'h_19',  //Will be used in the user feedback
+				nameForLogging : 'h_19', //Will be used in the logging
+				//An array of all media objects for this category.
+				mediaArray : [
+				    {word : 'withhold'}]
+
+			},
+			{
+				nameForFeedback : 'h_20',  //Will be used in the user feedback
+				nameForLogging : 'h_20', //Will be used in the logging
+				//An array of all media objects for this category.
+				mediaArray : [
+				    {word : 'take'}]
+
+			},
+			{
+				nameForFeedback : 'h_21',  //Will be used in the user feedback
+				nameForLogging : 'h_21', //Will be used in the logging
+				//An array of all media objects for this category.
+				mediaArray : [
+				    {word : 'table'}]
+
+			},
+			{
+				nameForFeedback : 'h_22',  //Will be used in the user feedback
+				nameForLogging : 'h_22', //Will be used in the logging
+				//An array of all media objects for this category.
+				mediaArray : [
+				    {word : 'uncharitable'}]
+
+			},
+			{
+				nameForFeedback : 'h_23',  //Will be used in the user feedback
+				nameForLogging : 'h_23', //Will be used in the logging
+				//An array of all media objects for this category.
+				mediaArray : [
+				    {word : 'chair'}]
+
+			},
+			{
+				nameForFeedback : 'h_24',  //Will be used in the user feedback
+				nameForLogging : 'h_24', //Will be used in the logging
+				//An array of all media objects for this category.
+				mediaArray : [
+				    {word : 'keep'}]
+
+			},
+			{
+				nameForFeedback : 'h_25',  //Will be used in the user feedback
+				nameForLogging : 'h_25', //Will be used in the logging
+				//An array of all media objects for this category.
+				mediaArray : [
+				    {word : 'desk'}]
+
+			},
+			{
+				nameForFeedback : 'h_26',  //Will be used in the user feedback
+				nameForLogging : 'h_26', //Will be used in the logging
+				//An array of all media objects for this category.
+				mediaArray : [
+				    {word : 'greedy'}]
+
+			},
+			{
+				nameForFeedback : 'h_27',  //Will be used in the user feedback
+				nameForLogging : 'h_27', //Will be used in the logging
+				//An array of all media objects for this category.
+				mediaArray : [
+				    {word : 'lamp'}]
+
+			},
+			{
+				nameForFeedback : 'h_28',  //Will be used in the user feedback
+				nameForLogging : 'h_28', //Will be used in the logging
+				//An array of all media objects for this category.
+				mediaArray : [
+				    {word : 'apathy'}]
+
+			},
+			{
+				nameForFeedback : 'h_29',  //Will be used in the user feedback
+				nameForLogging : 'h_29', //Will be used in the logging
+				//An array of all media objects for this category.
+				mediaArray : [
+				    {word : 'pencil'}]
+
+			},
+			{
+				nameForFeedback : 'h_30',  //Will be used in the user feedback
+				nameForLogging : 'h_30', //Will be used in the logging
+				//An array of all media objects for this category.
+				mediaArray : [
+				    {word : 'for me'}]
+
+			},
+			{
+				nameForFeedback : 'h_31',  //Will be used in the user feedback
+				nameForLogging : 'h_31', //Will be used in the logging
+				//An array of all media objects for this category.
+				mediaArray : [
+				    {word : 'retain'}]
+
+			},
+			{
+				nameForFeedback : 'h_32',  //Will be used in the user feedback
+				nameForLogging : 'h_32', //Will be used in the logging
+				//An array of all media objects for this category.
+				mediaArray : [
+				    {word : 'ceiling'}]
+
+			},
+			{
+				nameForFeedback : 'h_33',  //Will be used in the user feedback
+				nameForLogging : 'h_33', //Will be used in the logging
+				//An array of all media objects for this category.
+				mediaArray : [
+				    {word : 'book'}]
+
+			},
+			{
+				nameForFeedback : 'h_34',  //Will be used in the user feedback
+				nameForLogging : 'h_34', //Will be used in the logging
+				//An array of all media objects for this category.
+				mediaArray : [
+				    {word : 'selfish'}]
+
+			},
+			{
+				nameForFeedback : 'h_35',  //Will be used in the user feedback
+				nameForLogging : 'h_35', //Will be used in the logging
+				//An array of all media objects for this category.
+				mediaArray : [
+				    {word : 'mine'}]
+
+			}
+		],
+
+		base_url : 'https://maclabhub.github.io/moralcognition/habit'
+
+	});
+});
