914ecea786d9ee9c2cade962d07e90acd9dc075c
maclabhub <80420614+maclabhub@users.noreply.github.com>
2021-07-30 02:42:15 -0400

    Update habit_full_B1.js

--- habit/habit_full_B1.js
+++ habit/habit_full_B1.js
@@ -107,7 +107,7 @@ define(['pipAPI','underscore'], function(APIConstructor, _) {
 				image : 'https://baranan.github.io/minno-tasks/images/ampImages'
 			},
 
-			trialsInBlock : [28], //Number of trials in each block
+			trialsInBlock : [14], //Number of trials in each block
 			trialsInExample : 0, //Change to 0 if you don't want an example block
 
 			//Duration parameters.
